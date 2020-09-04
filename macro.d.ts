f1b5706839415bb70523f366d0f02ce81fdfb1f7
Eugene Chechuryn <e.chechurin@gmail.com>
2020-09-04 18:32:53 +0300

    add typings for proper cssed/macro import

---
+++ macro.d.ts
@@ -0,0 +1,6 @@
+declare const _default: any;
+export default _default;
+declare type CSSProperties = {
+    [key: string]: string | number | CSSProperties;
+};
+export declare function css(_strings: TemplateStringsArray, ..._exprs: Array<string | number | CSSProperties>): string;
